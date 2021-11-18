f93acd68e03d875f0c130c0c780161acaac579e4
RobRoger97 <RobertaRuggiero_97@hotmail.it>
2021-11-18 18:00:47 +0100

    Struct-Card [FIXED] Card

--- ApeAlFly/ApeAlFly/CardView.swift
+++ ApeAlFly/ApeAlFly/CardView.swift
@@ -7,12 +7,23 @@
 
 import SwiftUI
 
+extension Color {
+    
+}
+
 struct CardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 20)
                 .foregroundColor(.pink)
+                .frame(width: 323, height: 559)
+                .padding()
             Text("\(card.titolo)")
+                .bold()
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .padding(top)
+            
         }
     }
 }
