1bc85f4ec85e7ee14017f6dd5e1c90cc0343aa12
MarcoCucci <mcucci21@fed.idserve.net>
2021-11-18 16:23:56 +0100

    Starting Project

---
+++ ApeAlFly/ApeAlFly/ApeAlFlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApeAlFlyApp.swift
+//  ApeAlFly
+//
+//  Created by Marco Cucci  on 18/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ApeAlFlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
