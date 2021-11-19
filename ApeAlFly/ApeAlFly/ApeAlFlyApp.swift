0cd27c046e1ce80ebb44ef0f93728ba8ec30680b
RobRoger97 <RobertaRuggiero_97@hotmail.it>
2021-11-19 17:56:28 +0100

    Struct-Card [FIXED] CardView: colore e struttura della carta, posizione delle scritte e immagini nella carta, aggiunta della TabView

--- ApeAlFly/ApeAlFly/ApeAlFlyApp.swift
+++ ApeAlFly/ApeAlFly/ApeAlFlyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ApeAlFlyApp: App {
     var body: some Scene {
         WindowGroup {
-            CardView()
+            TotalView()
         }
     }
 }
--- ApeAlFly/ApeAlFly/Card.swift
+++ ApeAlFly/ApeAlFly/Card.swift
@@ -9,19 +9,19 @@ import Foundation
 import SwiftUI
 
 struct Card {
-    var testo : String
-    var titolo : String
-    var image : String
-    var border : String
-    var tag : String
-    var color : Color
+    let titolo : String
+    let testo : String
+    let image : String
+    let border : String
+    let tag : String
+    let color : Color
 }
 
-var first_card = Card(testo: "Prova del testo",titolo: "Prova del titolo", image: "circles", border:"border", tag: "script", color: Color.white)
-var second_card = Card(testo: "Prova del testo",titolo: "Prova del titolo", image: "circles", border:"border", tag: "script", color: Color.white)
-var third_card = Card(testo: "Prova del testo",titolo: "Prova del titolo", image: "circles", border:"border",tag: "script", color: Color.white)
-var fourth_card = Card(testo: "Prova del testo",titolo: "Prova del titolo", image: "circles",border:"border", tag: "script", color: Color.white)
-var fifth_card = Card(testo: "Prova del testo",titolo: "Prova del titolo", image: "circles", border:"border",tag: "script", color: Color.white)
-var sixth_card = Card(testo: "Prova del testo",titolo: "Prova del titolo", image: "circles", border:"border",tag: "script", color: Color.white)
-var seventh_card = Card(testo: "Prova del testo",titolo: "Prova del titolo", image: "circles", border:"border",tag: "script", color: Color.white)
-var eighth_card = Card(testo: "Prova del testo",titolo: "Prova del titolo", image: "circles", border:"border",tag: "script", color: Color.white)
+var first_card = Card(titolo: "Rianima gli oggetti",testo: "Riusciresti ad animare i seguenti cerchi? \n Cosa potrebbero diventare? \n Usa la tua creatività e divertiti a trasformare le seguenti figure in oggetti!", image: "circles", border:"border", tag: "script", color: Color.white)
+var second_card = Card(titolo: "Prova del testo",testo: "Prova del titolo", image: "circles", border:"border", tag: "script", color: Color.white)
+var third_card = Card(titolo: "Rianima gli oggetti",testo: "Prova del titolo", image: "circles", border:"border",tag: "script", color: Color.white)
+var fourth_card = Card(titolo: "Rianima gli oggetti",testo:"Prova del titolo", image: "circles",border:"border", tag: "script", color: Color.white)
+var fifth_card = Card(titolo: "Rianima gli oggetti",testo: "Prova del titolo", image: "circles", border:"border",tag: "script", color: Color.white)
+var sixth_card = Card(titolo: "Rianima gli oggetti",testo: "Prova del titolo", image: "circles", border:"border",tag: "script", color: Color.white)
+var seventh_card = Card(titolo: "Rianima gli oggetti",testo: "Prova del titolo", image: "circles", border:"border",tag: "script", color: Color.white)
+var eighth_card = Card(titolo: "Rianima gli oggetti",testo: "Prova del titolo", image: "circles", border:"border",tag: "script", color: Color.white)
